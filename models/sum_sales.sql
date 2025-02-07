c8826cbdb1052f7cf2695460d011f0b35c4ae45e
anu t <rajapokur@gmail.com>
2025-02-07 06:13:34 +0000

    hi

--- models/sum_sales.sql
+++ models/sum_sales.sql
@@ -1,4 +1,3 @@
-#use warehouse compute_wh;
 
 with sum_sal as(
 select job,deptno,sum(sal) from dbt_src.emp
---
+++ models/training/store_sales_profit_2023.sql
@@ -0,0 +1,15 @@
+with store_final as
+(
+select * from {{ref ('store_tbl_2023')}}
+),
+store_sales_final as
+(
+select * from {{ ref('store_Sales_tbl_2023')}}
+),
+Final_Sales_profit as
+(
+select s_store_name,s_county,sum(ss_sales_price),sum(ss_net_profit) from store_final,store_sales_final
+where s_store_sk=ss_store_sk
+group by 1,2
+)
+select * from final_sales_profit
