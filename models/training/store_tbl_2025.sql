ded634165733736dffc23cf2734eefd1b339e093
anu t <rajapokur@gmail.com>
2025-02-10 10:07:12 +0000

    gg

--- models/training/store_tbl_2025.sql
+++ models/training/store_tbl_2025.sql
@@ -1,6 +1,6 @@
 {{ config (materialized='table') }}
 with store_tbl_2025 as
 (
-    select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY FROM {{ source('DBT_SRC', 'Store_2023')}}
+    select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY FROM {{ source('DBT_SRC', 'STORE_2023')}}
 )
 select * from store_tbl_2025
