7a0270d9afcf8f1ce6612b1ad2715cd3eac4338c
anu t <rajapokur@gmail.com>
2025-02-14 09:14:41 +0000

    commit

--- models/example/trg_product.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='incremental',unique_key='PRODUCT_ID') }}
-
-select * from dbt_src.SRC_PRODUCT
-
-{% if inceremental() %}
-
-where update_dt>(select max(update_at) from {{ this }})
-
-{% endif %}
